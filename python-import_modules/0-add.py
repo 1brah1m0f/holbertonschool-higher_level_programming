5db41ae73aff9aead30ff7a0a49f740af01e1b21
1brah1m0f <sixiibrahimov217@gmail.com>
2025-12-01 11:21:28 +0000

    all

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def add(a, b):
-    print(f"{a} + {b} = {a+b}")
-a=1
-b=2
-add(a,b)
+    return a + b
+
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    print(f"{a} + {b} = {add(a, b)}")
