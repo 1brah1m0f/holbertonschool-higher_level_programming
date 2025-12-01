5a3d65ca95856957cac2ddedb9af97362f47d852
1brah1m0f <sixiibrahimov217@gmail.com>
2025-12-01 16:04:29 +0000

    1

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -2,6 +2,6 @@
 import sys
 if __name__ == "__main__":
     s=0
-    for i in range (1,len(sys.argv)):
-        s+=int(sys.argv[i])
+    for i in range (1, len(sys.argv)):
+        s= s + int(sys.argv[i])
     print(s)
