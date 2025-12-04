80b3a57dbec9eb365a27e252cf420633b8ea795b
1brah1m0f <sixiibrahimov217@gmail.com>
2025-12-04 12:59:45 +0000

    2

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-def multiple_returns(sentence,length):
-    if length == 0:
+def multiple_returns(sentence):
+    if sentence == "":
         first = None
         enght = len(sentence)
     else:
