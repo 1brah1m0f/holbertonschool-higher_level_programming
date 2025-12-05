cc0acc1486cdcf65d03e48756cc1180f6b9b3dde
1brah1m0f <sixiibrahimov217@gmail.com>
2025-12-05 13:10:54 +0000

    1

---
+++ python-import_modules/100-my_calculator.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+a,n,b=input.split()
+if n == "+":
+    print("{} {} {} = {}".format(a, n, b, add(a, b)))
+elif n == "-":
+    print("{} {} {} = {}".format(a, n, b, sub(a, b)))
+elif n == "-":
+    print("{} {} {} = {}".format(a, n, b, mul(a, b)))
+elif n == "-":
+    print("{} {} {} = {}".format(a, n, b, div(a, b)))
