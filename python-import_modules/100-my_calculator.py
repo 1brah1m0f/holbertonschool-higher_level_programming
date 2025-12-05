1965ffb2dd6e32e61398f00990af62acb924a989
1brah1m0f <sixiibrahimov217@gmail.com>
2025-12-05 13:26:34 +0000

    2

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 import sys
-import math
 from calculator_1 import add, sub, mul, div
 if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
     a , n, b = sys.argv[1], sys.argv[2], sys.argv[3]
     a, b = int(a), int(b)
     if n == "+":
@@ -13,3 +15,6 @@ if __name__ == "__main__":
         print("{} {} {} = {}".format(a, n, b, mul(a, b)))
     elif n == "/":
         print("{} {} {} = {}".format(a, n, b, div(a, b)))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
