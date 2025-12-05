d24aadafffd917ecb738624d7aa38b1d5fe4f179
1brah1m0f <sixiibrahimov217@gmail.com>
2025-12-05 13:21:06 +0000

    2

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
+import sys
 from calculator_1 import add, sub, mul, div
 if __name__ == "__main__":
-    a ,n, b = input().split()
+    a , n, b = sys.argv[1], sys.argv[2], sys.argv[3]
     a, b = int(a), int(b)
     if n == "+":
         print("{} {} {} = {}".format(a, n, b, add(a, b)))
     elif n == "-":
         print("{} {} {} = {}".format(a, n, b, sub(a, b)))
-    elif n == "*":
+    elif n == "^":
         print("{} {} {} = {}".format(a, n, b, mul(a, b)))
     elif n == "/":
         print("{} {} {} = {}".format(a, n, b, div(a, b)))
