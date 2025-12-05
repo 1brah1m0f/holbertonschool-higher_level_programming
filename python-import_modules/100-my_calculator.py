fb764e2849cb30968a26b74ca5847d9be6dd1789
1brah1m0f <sixiibrahimov217@gmail.com>
2025-12-05 13:27:20 +0000

    e

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,20 +1,24 @@
 #!/usr/bin/python3
 import sys
 from calculator_1 import add, sub, mul, div
+
 if __name__ == "__main__":
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    a , n, b = sys.argv[1], sys.argv[2], sys.argv[3]
-    a, b = int(a), int(b)
-    if n == "+":
-        print("{} {} {} = {}".format(a, n, b, add(a, b)))
-    elif n == "-":
-        print("{} {} {} = {}".format(a, n, b, sub(a, b)))
-    elif n == "*":
-        print("{} {} {} = {}".format(a, n, b, mul(a, b)))
-    elif n == "/":
-        print("{} {} {} = {}".format(a, n, b, div(a, b)))
+
+    a = int(sys.argv[1])
+    op = sys.argv[2]
+    b = int(sys.argv[3])
+
+    if op == "+":
+        print("{} + {} = {}".format(a, b, add(a, b)))
+    elif op == "-":
+        print("{} - {} = {}".format(a, b, sub(a, b)))
+    elif op == "*":
+        print("{} * {} = {}".format(a, b, mul(a, b)))
+    elif op == "/":
+        print("{} / {} = {}".format(a, b, div(a, b)))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
