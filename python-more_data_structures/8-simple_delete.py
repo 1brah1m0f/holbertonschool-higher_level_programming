3d0b14876c314363fe0d8f24284cf994d7da608b
1brah1m0f <sixiibrahimov217@gmail.com>
2025-12-08 23:15:39 +0000

    2

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-def simple_delete(a_dictionary, key=""):
-    if key in a_dictionary:
-        del a_dictionary[key]
-    return a_dictionary
+def multiply_by_2(a_dictionary):
+    for i in a_dictionary:
+        a_dictionary[f"{i}"] = int((a_dictionary[f"{i}"]*2))
