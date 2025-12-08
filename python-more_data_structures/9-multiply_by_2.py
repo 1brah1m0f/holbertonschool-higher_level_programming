3d5cf2de836371a7ab2bbee265519a193cda8d2c
1brah1m0f <sixiibrahimov217@gmail.com>
2025-12-08 23:19:19 +0000

    2

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     for i in a_dictionary:
-        a_dictionary[f"{i}"] = int((a_dictionary[f"{i}"]*2))
+        print(f"{i}: {a_dictionary{i}}")
     return a_dictionary
