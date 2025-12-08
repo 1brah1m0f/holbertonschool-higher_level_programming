5c18ec751f61c439d961c14bfa7a247a6fc6fda6
1brah1m0f <sixiibrahimov217@gmail.com>
2025-12-08 16:02:27 +0000

    2

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -3,4 +3,4 @@ def print_sorted_dictionary(a_dictionary):
     new = {}
     b = sorted(a_dictionary)
     for i in b:
-        print(f"{i}: ", a_dictionary[(f"{i}")])
+        print(f"{i}: {a_dictionary[i]}")
