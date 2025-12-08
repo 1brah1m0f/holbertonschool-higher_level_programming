f2b9d9d735e4630dd23352423d34d7c49cb0e23e
1brah1m0f <sixiibrahimov217@gmail.com>
2025-12-08 15:19:11 +0000

    2

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
-def only_diff_elements(set_1, set_2):
-    set3 = set(set_1 + set_2)
-    return set3
+def common_elements(set_1, set_2):
+    s = []
+    for i in set_1:
+        if i not in set_2:
+            s.append(i)
+    for i in set_2:
+        if i not in set_1:
+            s.append(i)
+    return s
