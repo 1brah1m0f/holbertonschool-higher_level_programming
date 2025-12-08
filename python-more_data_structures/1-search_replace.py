f57aee6c41d7587bfaea37d25c97b2a0ba3885ad
1brah1m0f <sixiibrahimov217@gmail.com>
2025-12-08 14:23:31 +0000

    2

---
+++ python-more_data_structures/1-search_replace.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    for i in my_list:
+        if i == search:
+            new_list.append(replace)
+        else:
+            new_list.append(i)
+    return new_list
