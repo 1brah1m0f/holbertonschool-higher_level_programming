e273c4486ddfbd4a2eecafe40021ea1df44c33d5
1brah1m0f <sixiibrahimov217@gmail.com>
2025-12-08 23:21:25 +0000

    1

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,5 @@
-#!/usr/bin/python3
 def multiply_by_2(a_dictionary):
+    new_dict = {}
     for i in a_dictionary:
-        print(f"{i}: {a_dictionary[i]}")
-    
+        new_dict[i] = a_dictionary[i] * 2
+    return new_dict
