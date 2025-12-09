dd2f6ffbee8825da4dcfa6a333627ce35daafc80
1brah1m0f <sixiibrahimov217@gmail.com>
2025-12-09 22:05:16 +0000

    1

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
+        if not a_dictionary:
+        return None
     s = ""
     s1 = 0
     for i in a_dictionary:
         if a_dictionary[i] > s1:
             s1 = a_dictionary[i]
             s = i
-    if s1 == 0:
-        s = None
     return s
