b39d60a3f3c114debd85f7831bc3f1da251d9d8f
1brah1m0f <sixiibrahimov217@gmail.com>
2025-12-09 22:00:40 +0000

    1

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     s = ""
+    s1 = 0
     for i in a_dictionary:
-        if a_dictionary[i] > s:
+        if a_dictionary[i] > s1:
+            s1 = a_dictionary[i]
             s = i
     return s
