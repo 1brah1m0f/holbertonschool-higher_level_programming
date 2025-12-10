390c462adbf0c87d3ff1f7135e87bcf09eceb2df
Şıxı <sixiibrahimov217@gmail.com>
2025-12-11 01:15:20 +0400

    2

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
     try:
-        result = ({} / {}).format(a, b)
+        result = a / b
     except ZeroDivisionError:
         return None
     finally:
         print("Operation attempted")
+    print("{:.2f}".format(result))
     return result
