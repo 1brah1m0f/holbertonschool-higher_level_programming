33e35a59ee3d89b946b23274352f6456dee70c4f
Şıxı <sixiibrahimov217@gmail.com>
2025-12-11 00:25:39 +0400

    2

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
+    a = []
     for i in my_list:
         try:
-            a = ("{:d}".format(i))
+            a.append("{:d}".format(i))
             count = count + 1
         except IndexError:
             pass
+    return a
     return count
