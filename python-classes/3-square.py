2c1bd86f1ba38367831db061f8287ee896b837c6
Şıxı <sixiibrahimov217@gmail.com>
2025-12-16 17:46:25 +0400

    2

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -4,6 +4,7 @@
 class Square:
     """hello"""
     def __init__(self, size=0):
+
         """Raises:
             TypeError: size must be an integer
             ValueError: Size must be >= 0"""
@@ -12,7 +13,9 @@ class Square:
 
         if size < 0:
             raise ValueError("size must be >= 0")
+        self.__size = size
+        
         def area(self):
             return self.__size * self.__size
 
-        self.__size = size
+
