18c570ae6d7a3cdb0d49fbfc9decc8447f4a9240
Şıxı <sixiibrahimov217@gmail.com>
2025-12-18 14:08:51 +0400

    2

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -5,17 +5,17 @@
 class Square:
     """hello"""
 
-    def __init__(self, size=0):
+    def __init__(self, area=0):
 
         """Raises:
             TypeError: size must be an integer
             ValueError: Size must be >= 0"""
-        if not isinstance(size, int):
+        if not isinstance(area, int):
             raise TypeError("size must be an integer")
 
-        if size < 0:
+        if area < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        self.__area = area
 
-    def area(self):
-        return self.__size ** 0.5
+    def size(self):
+        return self.__area ** 0.5
