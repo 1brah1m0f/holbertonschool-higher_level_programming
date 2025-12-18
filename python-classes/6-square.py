6f71365c63bc7d0f4ad2fdc794c85ab29d3a4ba2
Şıxı <sixiibrahimov217@gmail.com>
2025-12-18 21:06:51 +0400

    2

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -13,7 +13,7 @@ class Square:
                 raise ValueError("size must be >= 0")
         else:
             raise TypeError("size must be an integer")
-        self._position = position
+        self.position = position
 
 
     def area(self):
