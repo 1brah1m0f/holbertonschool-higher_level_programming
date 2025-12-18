bb2ad7dc353ddef2d5204dd6eb066d9cb0fe05af
Şıxı <sixiibrahimov217@gmail.com>
2025-12-19 00:33:50 +0400

    Refactor comments for clarity and consistency in Square class

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -7,10 +7,7 @@ class Square:
 
     def __init__(self, size=0, position=(0, 0)):
         if isinstance(size, int):
-#            sizenin int olub olmadiqin yoxlayir
             if size >= 0:
-#            sizenin musbet olub olmadiqin yoxlayir
-#             eger duzduse beraber edir sizeye eks halda valueError verir
                 self.__size = size
             else:
                 raise ValueError("size must be >= 0")
@@ -20,31 +17,16 @@ class Square:
 
     def area(self):
         return self.__size * self.__size
-#       sizenin areasin qaytarir
 
     def my_print(self):
         if self.__size == 0:
-# eger size 0 disa bos setir cap edib dovrden cixir
             print()
             return
         for _ in range(self.__position[1]):
-#           nece bos setir olacaqin gosterir 2 ci hissede
-#           yani yuxaridan asagi nece bosluq olacaq
-#           meselen eger bununu cavabi 2  (2, (0,2)) olsa
-# 
-# 
-#           ##
-#           ##
-#           bele olur cixis
             print()
 
         for i in range(self.__size):
             print(" " * self.__position[0] + "#" * self.__size)
-#          bu soldan saga nece bosluq olacaqin gosterir meselen
-#          (2, (1,0)) olsa burda 0 ci index 1 olur
-#          ##
-#          ## 
-#          bele olur cixis
 
     @property
     def position(self):
@@ -56,8 +38,6 @@ class Square:
                 len(value) != 2 or \
                 not all(isinstance(n, int) and n >= 0 for n in value):
             raise TypeError("position must be a tuple of 2 positive integers")
-        # eger positionun her iki deyiseninden biri 
-        # int yada + olmasa error verir
         self.__position = value
 
     @property
@@ -66,9 +46,6 @@ class Square:
 
     @size.setter
     def size(self, value):
-#        sizeni yoxlayir eger int yada menfi olsa erorr verir
-#        eger menfi olsa ValueError
-#        eger int olmasa TypeError verir
         if isinstance(value, int):
             if value >= 0:
                 self.__size = value
