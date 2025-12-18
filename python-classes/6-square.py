2172cd33f3633e9b30eb819da55da2475dffbc6c
Şıxı <sixiibrahimov217@gmail.com>
2025-12-18 20:54:43 +0400

    w

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+"""salam"""
+
+
 class Square:
+    """salan"""
+
     def __init__(self, size=0, position=(0, 0)):
         if isinstance(size, int):
             if size >= 0:
@@ -8,11 +13,10 @@ class Square:
                 raise ValueError("size must be >= 0")
         else:
             raise TypeError("size must be an integer")
-        
-
 
         def area(self):
             return self.__size * self.__size
+        
         def my_print(self):
             if self.__size == 0:
                 print()
