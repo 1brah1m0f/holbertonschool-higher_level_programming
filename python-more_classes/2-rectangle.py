d549f73c296109ecc5a32e86234a6d973f115230
Şıxı <sixiibrahimov217@gmail.com>
2025-12-21 22:10:04 +0400

    2

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -9,13 +9,13 @@ class Rectangle:
         self.height = height
     
     def area(self):
-        if (self.__width or self.__height) == 0:
+        if self.__width == 0 or self.__height == 0:
             return 0
         else:
             return self.__width * self.__height
 
     def perimeter(self):
-        if (self.__width or self.__height) == 0:
+        if self.__width == 0 or self.__height == 0:
             return 0
         else:
             return (self.__width + self.__height) * 2
