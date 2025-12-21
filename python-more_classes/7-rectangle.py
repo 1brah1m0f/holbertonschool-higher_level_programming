7204df51f1ce10be0f6bc33ea93ba06666f0b8fc
Şıxı <sixiibrahimov217@gmail.com>
2025-12-21 23:35:09 +0400

    1

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -55,7 +55,7 @@ class Rectangle:
     def __str__(self):
         """Düzbucaqlını '#' simvolu ilə təsvir edən string qaytarır"""
         if self.__width == 0 or self.__height == 0:
-            return
+            return ""
 
         rect_str = []
         for i in range(self.__height):
