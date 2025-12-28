7db68dcfbfdc3a7e1587dffa970c45d8c3a6b71f
1brah1m0f <sixiibrahimov217@gmail.com>
2025-12-28 21:40:14 +0400

    1

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -12,5 +12,5 @@ class BaseGeometry:
         if isinstance(name, str):
             if isinstance(value, int):
                 raise TypeError(f"{name} must be an integer")
-            if value <= 0:
-                raise ValueError(f"{name} must be greater than 0")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
