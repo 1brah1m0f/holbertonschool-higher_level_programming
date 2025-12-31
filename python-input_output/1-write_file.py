8a2376f0ea97e674ce2b2d1a211a877c0a97e4ad
1brah1m0f <sixiibrahimov217@gmail.com>
2025-12-31 23:31:31 +0400

    Fix indentation and update docstring in write_file function

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """hello i am Shixi Ibrahimov"""
 
+
 def write_file(filename="", text=""):
-     with open(filename, "r", encoding="utf-8") as f:
-          text += f.read()
-          text = len(text)
+    """i am Shixi"""
+    with open(filename, "r", encoding="utf-8") as f:
+        text += f.read()
+        text = len(text)
