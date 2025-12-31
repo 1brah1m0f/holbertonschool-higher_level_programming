5e424e74472bb2d748fce003fa52156167de63f0
1brah1m0f <sixiibrahimov217@gmail.com>
2025-12-31 23:06:22 +0400

    Update docstrings in read_file function and module

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""hm, I want to eat Paytaxt salad, but I should be coding"""
+
+
 def read_file(filename=""):
+    """mmmimimimim"""
+
     with open(filename,"r", encoding="utf-8") as f:
         print(f.read())
