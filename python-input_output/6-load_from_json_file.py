2d522f0009910b8b15e566a80a50fe5a406940e2
1brah1m0f <sixiibrahimov217@gmail.com>
2026-01-02 22:27:53 +0400

    2

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -7,5 +7,5 @@ import json
 
 def load_from_json_file(filename):
     """hm what is that"""
-    with open(filename, "w") as f:
+    with open(filename, "w" ,encoding="utf-8") as f:
         return json.load(f)
