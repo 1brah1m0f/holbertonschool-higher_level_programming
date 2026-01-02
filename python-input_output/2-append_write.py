fa34a702f152bf1a169a896407306596a36cff58
1brah1m0f <sixiibrahimov217@gmail.com>
2026-01-02 16:29:50 +0400

    2

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""this is """
+
+
+def append_write(filename="", text=""):
+    """mimim"""
+
+    with open(filename, "r", encoding="utf-8") as s:
+        return s.write(text)
