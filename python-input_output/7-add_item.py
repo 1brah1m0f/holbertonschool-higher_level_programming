558fe6bb920796fbb63dadc02b5a479ef57bafaa
1brah1m0f <sixiibrahimov217@gmail.com>
2026-01-02 22:48:59 +0400

    Refactor add_item.py to improve docstring and correct filename assignment

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,8 +1,21 @@
 #!/usr/bin/python3
-"""mimimimi"""
+"""Add all arguments to a Python list and save them to a file"""
 
+import sys
 
+# import the functions from those weirdly named files
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-add_item.json = save_to_json_file + load_from_json_file
+filename = "add_item.json"
+
+args = sys.argv[1:]
+
+try:
+    my_list = load_from_json_file(filename)
+except FileNotFoundError:
+    my_list = []
+
+my_list.extend(args)
+
+save_to_json_file(my_list, filename)
