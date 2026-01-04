781e2d4ce2e68b47b6022a2767567c9f8c144b47
1brah1m0f <sixiibrahimov217@gmail.com>
2026-01-04 23:02:43 +0400

    1

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python3
 """hm"""
 
+import json
+
+def serialize_and_save_to_file(data, filename):
+    """mimim"""
+    with open(filename, 'w') as file:
+        json.dump(data, file)
+
+def load_and_deserialize(filename):
+    """mimimi"""
+    with open(filename, 'r') as file:
+        return json.load(file)
