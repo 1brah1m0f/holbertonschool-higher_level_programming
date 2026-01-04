804f0f779286028efed4249e07fbc99e2fb0486b
1brah1m0f <sixiibrahimov217@gmail.com>
2026-01-04 22:26:46 +0400

    1

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -5,20 +5,19 @@
 class Student:
     """1"""
 
-
     def __init__(self, first_name, last_name, age):
         """mm"""
 
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-    
+
     def to_json(self, attrs=None):
         """mimimim"""
 
         if attrs == None:
             return self.__dict__
-        elif type(attrs) == list:
+        elif isinstance(attrs, list):
             result = {}
             for i in attrs:
                 if i in self.__dict__:
