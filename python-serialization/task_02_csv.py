15552df7aa8a4ade9ec5fc0a258b085d46c12e68
1brah1m0f <sixiibrahimov217@gmail.com>
2026-01-10 00:03:45 +0400

    2121212

---
+++ python-serialization/task_02_csv.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""hm"""
+
+import csv
+import json
+
+def convert_csv_to_json(filename):
+    try:
+        with open(filename, "r", encoding="utf-8") as f:
+            reader = csv.DictReader(f)
+            data = list(reader)
+
+        with open("data.json", "w", encoding="utf-8") as jf:
+            json.dump(data, jf)
+
+        return True
+
+    except:
+        return False
