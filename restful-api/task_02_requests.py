2e9afd081303907842d059cd6c9688289d54c4d9
1brah1m0f <sixiibrahimov217@gmail.com>
2026-01-10 16:46:40 +0400

    2

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Docstring for restful-api.task_02_requests"""
+
+import json
+import requests
+import csv
+
+response = requests.get('https://jsonplaceholder.typicode.com/posts')
+
+
+def fetch_and_print_posts():
+    print("Status code:", response.status_code)
+    if response.status_code == 200:
+        data = response.json()
+        for i in data:
+            print(i['title'])
+
+
+def fetch_and_save_posts():
+    data = response.json()
+    with open(data, "w",encoding="utf-8") as f:
+        fieldnames = ['id', 'title', 'body']
+        writer = csv.DictWriter(f, fieldnames=fieldnames)
+        writer.writeheader()
+        for post in data:
+            writer.writerow({
+                'id': post['id'],
+                'title': post['title'],
+                'body': post['body']
+            })
