c37de538752d713572c40c77a9b5d5164865aca5
1brah1m0f <sixiibrahimov217@gmail.com>
2026-02-13 16:22:30 +0400

    1

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const a = process.argv[2]
-const b = process.argv[3]
+const a = process.argv[2];
+const b = process.argv[3];
 
-console.log(a + ' is ' + b)
+console.log(a + ' is ' + b);
