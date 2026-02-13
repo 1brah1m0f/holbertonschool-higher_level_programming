ca40d78d766f471bd147f63a27e51fd2209f5d12
1brah1m0f <sixiibrahimov217@gmail.com>
2026-02-13 17:48:29 +0400

    feat: add 8-square.js to print a square of X based on user input

---
+++ javascript-warm_up/8-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const x = Number(process.argv[2]);
+let x1 = x
+let i = 0;
+let a =''
+while (x1 > 0) {
+    a = a + "X";
+    x1--;
+}
+if (isNaN(x)) {
+    console.log('Missing size')
+} else {
+    while (i < x) {
+        console.log(a);
+        i++;
+    }
+}
