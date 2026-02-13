f7dd6740b166c50ffbd10fe919b7d7f1622c3fbb
1brah1m0f <sixiibrahimov217@gmail.com>
2026-02-13 18:10:35 +0400

    fix: correct factorial calculation in 10-factorial.js

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const a = Number(process.argv[2]);
+let i = 1;
+let f = 1;
+if (isNaN(a)) {
+  console.log('1');
+} else {
+  while (i < a) {
+    f = f * s;
+    i++;
+  }
+  console.log(f)
+}
