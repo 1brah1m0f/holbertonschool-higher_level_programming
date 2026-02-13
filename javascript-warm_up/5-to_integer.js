60c08caac6a0b2144629da2df9ef45a2bc845259
1brah1m0f <sixiibrahimov217@gmail.com>
2026-02-13 16:47:22 +0400

    1

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-let a = process.argv[2]
+let a = process.argv[2];
 if (a === String) {
-    console.log('Not a number')
+    console.log('Not a number');
 } else {
-    console.log('My number: ' + a)
+    console.log('My number: ' + a);
 }
