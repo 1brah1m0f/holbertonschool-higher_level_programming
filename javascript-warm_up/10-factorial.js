f005b4c21b6b803cb1640aed5d73e5daeab58387
1brah1m0f <sixiibrahimov217@gmail.com>
2026-02-13 18:17:00 +0400

    1

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -4,13 +4,13 @@ let i = 1;
 let f = 1;
 function fact (i) {
   if (isNaN(a)) {
-  console.log('1');
+    console.log('1');
 } else {
-  while (i <= a) {
-    f = f * i;
-    i++;
-  }
-  console.log(f)
+    while (i <= a) {
+      f = f * i;
+      i++;
+    }
+    console.log(f)
 }
 }
 fact(i)
